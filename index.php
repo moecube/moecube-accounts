4355b4ffe1ca8107e9576e904ef5a85ad11acc0a
smdcn <smdcnnet@gmail.com>
2013-10-25 16:28:19 +0800

    目录调整

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-    define('MODE_NAME', 'rest');
-    define('APP_DEBUG', true);
-    //加载框架入口文件
-    require './ThinkPHP/ThinkPHP.php';
---
+++ wwwroot/index.php
@@ -0,0 +1,5 @@
+<?php
+	define('MODE_NAME', 'rest');
+	define('APP_DEBUG',TRUE);
+	define('APP_PATH','../application/');
+	require '../framework/ThinkPHP.php';
