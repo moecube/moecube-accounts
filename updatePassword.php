8fa5f04f8609a868841f5aa2241b29e027dbdd62
nano <ncysatnaf@gmail.com>
2017-03-15 17:24:55 +0800

    fix password

--- updatePassword.php
+++ updatePassword.php
@@ -1,41 +1,34 @@
 <?php
-	require_once "config.php";
+require_once "config.php";
 
-	$key		=$_GET	['key'];
-	$username	=$_GET	['username'];
-	$password	=$_POST	['password'];
-	$password2	=$_POST	['password2'];
+if (!isset($_GET['key']) || !isset($_GET['user_id']) || !isset($_POST['password'])) {
+    http_response_code(400);
+    die(json_encode(["message" => '参数不完整']));
+}
 
+$key = $_GET['key'];
+$user_id = $_GET['user_id'];
+$password = $_POST['password'];
 
-    $sql='SELECT * FROM forgotpassword WHERE key=:key AND username=:username';
-    $sth=$pdo->prepare($sql,array(PDO::ATTR_CURSOR=>PDO::CURSOR_FWDONLY));
-    $sth->execute(array(':key'=>$key,':username'=>$username));
-    $s=$sth->fetchAll();
-    if(count($s)){
-    	if($password!=null){
-    		if($password!=$password2){
-    			echo '密码不一致';
-    		}else{
-    			$sql='UPDATE users SET password=:password WHERE username=:username';
-    			$sth=$pdo->prepare($sql,array(PDO::ATTR_CURSOR=>PDO::CURSOR_FWDONLY));
-    			$sth->execute(array(':password'=>$password,':username'=>$username));
-    			$s2=$sth->fetchAll();
+$sql = 'SELECT * FROM forgotpassword WHERE key=:key AND user_id=:user_id';
+$sth = $pdo->prepare($sql);
+$sth->execute(array(':key' => $key, ':user_id' => $user_id));
+$user = $sth->fetch();
+if ($user) {
 
-    			if(count($s2)){
-    				echo '密码修改成功';
-    				$sql='DELETE FROM forgotpassword WHERE username=:username';
-    				$sth=$pdo->prepare($sql,array(PDO::ATTR_CURSOR=>PDO::CURSOR_FWDONLY));
-    				$sth->execute(array(':username'=>$username));
-    			}else{
-    				echo '密码修改失败';
-    			}
-    		}
-    	}else{
-    		echo '页面';
-    		require_once 'updatePassword.html';
-    	}
-    }else{
-		var_dump($s);
-	    die('妈了个鸡,你想干嘛');
-	}
-?>
+    $password = hash_pbkdf2("sha256", $password, $user["salt"], 64000);
+
+    $sql = 'UPDATE users SET password_hash=:password WHERE user_id=:user_id';
+    $sth = $pdo->prepare($sql);
+    $sth->execute(array(':password' => $password, ':user_id' => $user_id));
+
+    echo '密码修改成功';
+    $sql = 'DELETE FROM forgotpassword WHERE user_id=:user_id';
+    $sth = $pdo->prepare($sql);
+    $sth->execute(array(':user_id' => $user_id));
+
+    http_response_code(204);
+} else {
+    http_response_code(400);
+    die(json_encode(["message" => '找不到该用户']));
+}
