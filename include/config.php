279de30e77ff89d26232e23d7c2745dfdb3ac7df
zh99998 <zh99998@gmail.com>
2017-03-17 15:55:58 +0800

    fix

--- include/config.php
+++ include/config.php
@@ -1,6 +1,6 @@
 <?php
 ini_set("display_erros", false);
-require_once "../vendor/autoload.php";
+require_once "./vendor/autoload.php";
 require_once './sendmail.php';
 
 $dotenv = new Dotenv\Dotenv(__DIR__);
@@ -8,6 +8,5 @@ $dotenv->load();
 
 $db = new PDO(getenv("DATABASE"));
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 
 $upload_target = "uploads";
