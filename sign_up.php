539aa8d6b4a2ec1ca65d7a8fa253f97026ea9305
zh99998 <zh99998@gmail.com>
2017-03-17 17:32:34 +0800

    default avatar

--- sign_up.php
+++ sign_up.php
@@ -10,41 +10,46 @@ $password2 = $_POST['password2'];
 $submit = $_POST['submit'];
 
 $json_arr = [];
-if ($email != NULL) {
+if ($email != null) {
     emailOK($db, $email);
 }
-if ($username != NULL) {
+if ($username != null) {
     usernameOK($db, $username);
 }
 if ($submit == 'true') {
-    $ok = TRUE;
-    if ($email == NULL) {
+    $ok = true;
+    if ($email == null) {
         $json_arr['email'] = ["class" => "red", "html" => "邮箱地址不能为空"];
-        $ok = FALSE;
+        $ok = false;
     }
-    if ($username == NULL) {
+    if ($username == null) {
         $json_arr['username'] = ["class" => "red", "html" => "用户名不能为空"];
-        $ok = FALSE;
+        $ok = false;
     }
-    if ($password == NULL) {
+    if ($password == null) {
         $json_arr['password'] = ["class" => "red", "html" => "密码不能为空"];
-        $ok = FALSE;
+        $ok = false;
     }
-    if ($password2 == NULL) {
+    if ($password2 == null) {
         $json_arr['password2'] = ["class" => "red", "html" => "确认密码不能为空"];
-        $ok = FALSE;
+        $ok = false;
     }
 
     if ($ok) {
         $sql = 'INSERT INTO users(username, password, email, nickname) VALUES(:username, :password, :email, :nickname)';
         $sth = $db->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
-        $sth->execute([':username' => $username, ':password' => $password, ':email' => $email, ':nickname' => $nickname]);
+        $sth->execute([
+            ':username' => $username,
+            ':password' => $password,
+            ':email'    => $email,
+            ':nickname' => $nickname
+        ]);
         $data1 = $sth->fetchAll();
         if (count($data1)) {
             $title = "感谢乃注册MoeCube账号";
             $body = "单击链接 或将链接复制到网页地址栏并回车 来激活账号 http://192.168.1.124/activate.php?username=$username";
             sendMail($email, $title, $body);
-            $json_arr['success'] = TRUE;
+            $json_arr['success'] = true;
         }
     }
 }
