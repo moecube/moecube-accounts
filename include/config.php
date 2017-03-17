6c87d60a3c23e247536a95c9cde5e579bc99959d
zh99998 <zh99998@gmail.com>
2017-03-17 16:01:05 +0800

    fix

--- include/config.php
+++ include/config.php
@@ -3,8 +3,8 @@ ini_set("display_erros", false);
 require_once "vendor/autoload.php";
 require_once 'include/sendmail.php';
 
-$dotenv = new Dotenv\Dotenv(__DIR__);
-$dotenv->load();
+//$dotenv = new Dotenv\Dotenv(__DIR__);
+//$dotenv->load();
 
 $db = new PDO(getenv("DATABASE"));
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
